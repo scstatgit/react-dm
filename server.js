23442379acbd537b9d703beee73b63cdc165fde6
scstatgit <seungchan.statistics@gmail.com>
2024-08-31 19:16:21 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://placehold.co/60x60/png',
-        'name': '홍길동', 
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '대학생'
-        },
-        {
-        'id': 2,
-        'image': 'http://picsum.photos/id/427/60/60',
-        'name': '김철수', 
-        'birthday': '960301',
-        'gender': '남자',
-        'job': '직업군인'
-        },
-        {
-        'id': 3,
-        'image': 'http://picsum.photos/id/428/60/60',
-        'name': '이영희', 
-        'birthday': '960509',
-        'gender': '여자',
-        'job': '직장인'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
