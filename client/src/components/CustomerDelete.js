fbde4352d197dbf0803756671b4012f0a869f37e
scstatgit <seungchan.statistics@gmail.com>
2024-09-03 22:38:43 +0900

    Add Customer Delete Module

---
+++ client/src/components/CustomerDelete.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class CustomerDelete extends React.Component {
+
+    // RestAPI에서 delete하는 방법
+    deleteCustomer(id) {
+        const url = '/api/customers/' + id;
+        fetch(url, {
+            method: 'DELETE'
+        });
+        this.props.stateRefresh();
+    }
+
+    render() {
+        return (
+            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</button>
+        )
+    }
+}
+
+export default CustomerDelete;
