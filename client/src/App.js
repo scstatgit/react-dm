c499f128b37a939feb3587512c140b6a94ce647c
scstatgit <seungchan.statistics@gmail.com>
2024-09-05 01:06:29 +0900

    Add Material UI Design for Add/Delete

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { CircularProgress } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
+const theme = unstable_createMuiStrictModeTheme();
 
 const styles = theme => ({
   root: {
@@ -68,33 +71,35 @@ class App extends Component {
     const { classes } = this.props;
     return (
       <div>
-        <Paper className={classes.root}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers ? this.state.customers.map(c => {
-                return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
-              }) : 
-              <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
-                </TableCell>
-              </TableRow>
-              }
-            </TableBody>
-          </Table>
-        </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
+        <ThemeProvider theme = {theme}>
+          <Paper className={classes.root}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>번호</TableCell>
+                  <TableCell>이미지</TableCell>
+                  <TableCell>이름</TableCell>
+                  <TableCell>생년월일</TableCell>
+                  <TableCell>성별</TableCell>
+                  <TableCell>직업</TableCell>
+                  <TableCell>설정</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.customers ? this.state.customers.map(c => {
+                  return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+                }
+              </TableBody>
+            </Table>
+          </Paper>
+          <CustomerAdd stateRefresh={this.stateRefresh}/>
+        </ThemeProvider>
       </div>
     );
   }
--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import post from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
+const theme = unstable_createMuiStrictModeTheme();
+
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
+
 
 class CustomerAdd extends React.Component {
 
@@ -11,7 +29,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     }
 
@@ -28,7 +47,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
     }
 
@@ -62,19 +82,56 @@ class CustomerAdd extends React.Component {
         return post.post(url, formData, config) // axios: 서버로 데이터 전송
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
+    }
+
     render() {
+        const { classes } = this.props;
         return (
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가하기</button>
-            </form>
+            <div>
+                <ThemeProvider theme = {theme}>
+                    <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                        고객추가하기
+                    </Button>
+                    <Dialog open={this.state.open} onClose={this.handleClose}>
+                        <DialogTitle>고객 추가</DialogTitle>
+                        <DialogContent>
+                            <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                            <label htmlFor="raised-button-file">
+                                <Button variant="contained" color="primary" component="span" name="file">
+                                    {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                                </Button>
+                            </label>
+                            <br/>
+                            <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></TextField><br/>
+                            <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></TextField><br/>
+                            <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}></TextField><br/>
+                            <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></TextField><br/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                            <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                        </DialogActions>
+                    </Dialog>
+                </ThemeProvider>
+            </div>
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
