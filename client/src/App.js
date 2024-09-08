411c7d8483773d3c6c616e52e54c2c01e567b314
scstatgit <seungchan.statistics@gmail.com>
2024-09-08 10:39:44 +0900

    Update Dependency Problem

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,68 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import CustomerAdd from './components/CustomerAdd';
 import './App.css';
-import Paper from '@material-ui/core/Paper'
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import { CircularProgress } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { ThemeProvider } from "@material-ui/core/styles";
-import { unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
-const theme = unstable_createMuiStrictModeTheme();
+import { Paper, Table, TableHead, TableBody, TableRow, TableCell, CircularProgress, AppBar, Toolbar, IconButton, Typography, InputBase } from '@mui/material';
+import { ThemeProvider, createTheme, alpha } from '@mui/material/styles';
+import { withStyles } from '@mui/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const theme = createTheme();
 
 const styles = theme => ({
   root: {
+    width: '100%'
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
     width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto'
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
   },
-  table: {
-    minWidth: 1080
+  searchIcon: {
+    padding: theme.spacing(9),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  progress: {
-    margin: theme.spacing(2)
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 10),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
   }
 });
 
@@ -34,14 +73,22 @@ class App extends Component {
     super(props);
     this.state = {
       customer: '',
-      completed: 0
+      completed: 0,
+      searchKeyword : ''
     }
   }
 
+  handleValueChange = (e) =>{
+    let nextState ={};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  } 
+    
   stateRefresh = () => {
     this.setState({
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword : ''
     });
     // 고객데이터 불러오기
     this.callApi()
@@ -49,11 +96,15 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
   callApi = async () => {
@@ -70,39 +121,62 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <ThemeProvider theme = {theme}>
-          <Paper className={classes.root}>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>번호</TableCell>
-                  <TableCell>이미지</TableCell>
-                  <TableCell>이름</TableCell>
-                  <TableCell>생년월일</TableCell>
-                  <TableCell>성별</TableCell>
-                  <TableCell>직업</TableCell>
-                  <TableCell>설정</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.customers ? this.state.customers.map(c => {
-                  return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
-                }) : 
-                <TableRow>
-                  <TableCell colSpan="6" align="center">
-                    <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
-                  </TableCell>
-                </TableRow>
-                }
-              </TableBody>
-            </Table>
-          </Paper>
-          <CustomerAdd stateRefresh={this.stateRefresh}/>
-        </ThemeProvider>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+             고객 관리 시스템
+            </Typography>
+            <div className={classes.grow}/>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="검색하기" classes={{ root: classes.inputRoot, input: classes.inputInput }}/>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c => {
+                return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(App);
+const AppWithStyles = withStyles(styles)(App);
+
+const AppWithTheme = (props) => (
+  <ThemeProvider theme={theme}>
+    <AppWithStyles {...props} />
+  </ThemeProvider>
+);
+
+export default AppWithTheme;
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { ThemeProvider } from "@material-ui/core/styles";
-import { unstable_createMuiStrictModeTheme } from '@material-ui/core/styles';
-const theme = unstable_createMuiStrictModeTheme();
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 class CustomerDelete extends React.Component {
 
@@ -42,23 +42,21 @@ class CustomerDelete extends React.Component {
     render() {
         return (
             <div>
-                <ThemeProvider theme = {theme}>
-                    <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
-                    <Dialog open={this.state.open} onClose={this.handleClose}>
-                        <DialogTitle onClose={this.handleClose}>
-                            삭제 경고
-                        </DialogTitle>
-                        <DialogContent>
-                            <Typography gutterBottom>
-                                선택한 고객 정보가 삭제됩니다.
-                            </Typography>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
-                            <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
-                        </DialogActions>
-                    </Dialog>
-                </ThemeProvider>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle onClose={this.handleClose}>
+                        삭제 경고
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            선택한 고객 정보가 삭제됩니다.
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         )
     }
